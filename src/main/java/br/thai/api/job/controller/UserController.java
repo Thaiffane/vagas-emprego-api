e865a94654f2a5fc48269ef536b629422c07b7bb
thaiffane <thaiffanect43nascimento@gmail.com>
2024-06-15 14:53:19 -0300

    Criando esqueleto

---
+++ src/main/java/br/thai/api/job/controller/UserController.java
@@ -0,0 +1,21 @@
+package br.thai.api.job.controller;
+
+public class UserController {
+    //POST, GET, PUT, DELETE, OPTIONS
+    //CRUD,
+    public void create(){
+
+    }
+
+    public  void listAll(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public delete(){
+        
+    }
+}
---
+++ src/main/java/br/thai/api/job/domain/user/User.java
@@ -0,0 +1,12 @@
+package br.thai.api.job.domain.user;
+
+import br.thai.api.job.domain.user.address.Address;
+
+public class User {
+    private Long id;
+    private String name;
+    private String cpf;
+    private Address address;
+
+
+}
